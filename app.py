9b2412f9ff423ee7aa915a0330d3fe8dc0d18f05
nschloesserm <nicholasmckennaart@gmail.com>
2022-10-08 23:48:49 +0000

    add form and sent info to admin page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -23,7 +23,16 @@ def booktable():
 def contact():
     return render_template("contact.html")
 
-
+@app.route("/admin", methods=["POST"])
+def admin():
+    full_name = request.form.get("full_name")
+    email = request.form.get("email")
+    telephone = request.form.get("telephone")
+    date = request.form.get("date")
+    party_size = request.form.get("party_size")
+    time = request.form.get("time")
+    message = request.form.get("message")
+    return render_template("admin.html", full_name=full_name, email=email, telephone=telephone, date=date, party_size=party_size, time=time, message=message)
 
 if __name__ == "__main__":
     app.run(
