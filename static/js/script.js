9a19cf7e676e4eaa0244c0ae2cbe2ac9040aeb45
nschloesserm <nicholasmckennaart@gmail.com>
2022-10-10 21:29:43 +0000

    add new id fuction to javascript

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,30 @@ function checkDate() {
     var y = [...document.querySelectorAll("h6")].map(e=>e.innerHTML);
     document.getElementById("selectDate").innerHTML = x + y;
     var arrayLength = y.length
-    for (var h = 0; h < arrayLength; h++) {
-        
-    }
     for (var i = 0; i < arrayLength; i++) {
-        if (x != y[i]) {
-            document.getElementsByClassName(y[i]).style.display = "none";
+       /* if (x != y[i]) {
+            document.getElementsByClassName("cust" + (i + 1)).style.display = "none";
         } else {
-            document.getElementsByClassName(y[i]).style.display = "block";
-        }
-    }
+            document.getElementsByClassName("cust" + (i + 1)).style.display = "block";
+        }  */
+        console.log("cust" + (i + 1));
+        console.log(y[i]);
+            }
 }
+
+function changeID() {
+    var y = [...document.querySelectorAll("h6")].map(e=>e.innerHTML);
+    var arrayLength = y.length
+    for (var j = 0; j < arrayLength; j++) {
+        if (element = document.getElementById("customersection")) {
+            element.id = "cust" + (j + 1); 
+        } 
+    }   
+  }
+  
+  let element = document.getElementById("customersection");
+  changeID();
+  
+
+
+
