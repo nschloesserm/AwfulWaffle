cabb7aa54522f3c37f3db12731e487ee234720f1
nschloesserm <nicholasmckennaart@gmail.com>
2022-10-10 23:02:08 +0000

    change reservation times

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,26 @@
 function checkDate() {
     var x = document.getElementById("dateform").value;
-    var y = [...document.querySelectorAll("h6")].map(e=>e.innerHTML);
-    document.getElementById("selectDate").innerHTML = x + y;
+    var y = [...document.querySelectorAll("h6")].map(e => e.innerHTML);
+    document.getElementById("selectDate").innerHTML = x;
     var arrayLength = y.length
     for (var i = 0; i < arrayLength; i++) {
         if (x != y[i]) {
             document.getElementById("cust" + (i + 1)).style.display = "none";
         } else {
             document.getElementById("cust" + (i + 1)).style.display = "block";
-        }  
-        console.log("cust" + (i + 1));
-        console.log(y[i]);
-            }
+        }
+    }
 }
 
 function changeID() {
-    var y = [...document.querySelectorAll("h6")].map(e=>e.innerHTML);
+    var y = [...document.querySelectorAll("h6")].map(e => e.innerHTML);
     var arrayLength = y.length
     for (var j = 0; j < arrayLength; j++) {
         if (element = document.getElementById("customersection")) {
-            element.id = "cust" + (j + 1); 
-        } 
-    }   
-  }
-  
-  let element = document.getElementById("customersection");
-  changeID();
-  
+            element.id = "cust" + (j + 1);
+        }
+    }
+}
 
-
-
+let element = document.getElementById("customersection");
+changeID();
