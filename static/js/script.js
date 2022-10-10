6bf1582392bd2f12022ac2734811772e57182e82
nschloesserm <nicholasmckennaart@gmail.com>
2022-10-10 18:44:29 +0000

    add javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,16 @@
 function checkDate() {
     var x = document.getElementById("dateform").value;
-    /* var y = document.getElementById("{{customer.resdate}}").innerHTML; */
-    document.getElementById("selectDate").innerHTML = x;
-    /* if (x != y) {
-        document.getElementById("customer{{customer.id}}").style.display = "none";
-    } else {
-        document.getElementById("customer{{customer.id}}").style.display = "block";
-    } */
+    var y = [...document.querySelectorAll("h6")].map(e=>e.innerHTML);
+    document.getElementById("selectDate").innerHTML = x + y;
+    var arrayLength = y.length
+    for (var h = 0; h < arrayLength; h++) {
+        
+    }
+    for (var i = 0; i < arrayLength; i++) {
+        if (x != y[i]) {
+            document.getElementsByClassName(y[i]).style.display = "none";
+        } else {
+            document.getElementsByClassName(y[i]).style.display = "block";
+        }
+    }
 }
